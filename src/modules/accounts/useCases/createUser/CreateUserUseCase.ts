a65f7df5a4e7596a0b2fbeaf345fb81a68b6a90b
Leonardo <leonardo@zungasoft.net>
2021-04-05 07:38:38 -0300

    class Error

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,6 +1,7 @@
 import { hash } from "bcrypt";
 import { inject, injectable } from "tsyringe";
 
+import { AppError } from "../../../../errors/AppError";
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
@@ -20,7 +21,7 @@ class CreateUserUseCase {
     const usersAlreadyExists = await this.usersRepository.findByEmail(email);
 
     if (usersAlreadyExists) {
-      throw new Error("E-mail already exists!");
+      throw new AppError("E-mail already exists!");
     }
 
     const passwordHash = await hash(password, 8);
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors";
 import swaggerUi from "swagger-ui-express";
 
 import "./database";
 
 import "./shared/container";
 
+import { AppError } from "./errors/AppError";
 import { router } from "./routes";
 import swaggerFile from "./swagger.json";
 
@@ -16,8 +18,19 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
 
-app.get("/", (request, response) => {
-  return response.json({ message: "hello world" });
-});
+app.use(
+  (err: Error, request: Request, response: Response, next: NextFunction) => {
+    if (err instanceof AppError) {
+      return response.status(err.statusCode).json({
+        message: err.message,
+      });
+    }
 
-app.listen(3333);
+    return response.status(500).json({
+      status: "error",
+      message: `Internal server error = ${err.message}`,
+    });
+  }
+);
+
+app.listen(3333, () => console.log("server is running"));
